88a3b40fc7011b651c84f5783229eaa7a8fe6461
Ren-Mor <renatomorelli@hotmail.it>
2025-03-26 16:32:25 +0100

    new file

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const addTaskButton = document.getElementById("new-task");
+  const taskInput = document.getElementById("insert-task");
+  const taskList = document.getElementById("task-list");
+
+  addTaskButton.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Cancella";
+      deleteButton.classList.add("Cancella");
+      taskItem.appendChild(deleteButton);
+
+      deleteButton.addEventListener("click", function (e) {
+        taskList.removeChild(taskItem);
+      });
+
+      taskList.appendChild(taskItem);
+
+      taskInput.value = "";
+    } else {
+      alert("Aggiungi un promemoria.");
+    }
+  });
+
+  taskInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      addTaskButton.click();
+    }
+  });
+});
